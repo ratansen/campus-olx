f6572554f885d95b7c6db33b93eabc6653452a2e
Ratan-Sen <ratansen.iitg@gmail.com>
2022-05-21 16:08:19 +0530

    crud implemented

--- api/views.py
+++ api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ProductViews(APIView):
+
+    #post
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # get
+    def get(self, request, id=None):
+        if id:
+            item = Product.objects.get(id=id)
+            serializer = ProductSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+        items = Product.objects.all()
+        serializer = ProductSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    # update
+    def patch(self, request, id=None):
+        item = Product.objects.get(id=id)
+        serializer = ProductSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+    
+    # delete
+    def delete(self, request, id=None):
+        item = get_object_or_404(Product, id=id)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
